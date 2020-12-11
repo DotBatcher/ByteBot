ab304d0b5c0fab97256d93d7f3d7ecdded13d688
DotBatcher <59958734+DotBatcher@users.noreply.github.com>
2020-12-11 03:11:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,26 +51,22 @@ client.on('message', message => {
 
 
 client.on('message', message => {
-  // Ignore messages that aren't from a guild
+  
   if (!message.guild) return;
 
-  // If the message content starts with "!kick"
+  
   if (message.content.startsWith('!kick')) {
     if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send('You do not have permision!'); console.log(`Failed attempt to kick a user.`);
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    
+    
     const user = message.mentions.users.first();
-    // If we have a user mentioned
+    
     if (user) {
-      // Now we get the member from the user
+      
       const member = message.guild.member(user);
-      // If the member is in the guild
+      
       if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
+        
         member
           .kick('')
           .then(() => {
@@ -79,18 +75,14 @@ client.on('message', message => {
             console.log(`kicked ${user.tag}`);
           })
           .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to kick the member,
-            // either due to missing permissions or role hierarchy
             message.reply('I was unable to kick the member');
-            // Log the error
             console.error(err);
           });
       } else {
-        // The mentioned user isn't in this guild
+        
         message.reply("That user isn't in this guild!");
       }
-      // Otherwise, if no user was mentioned
+      
     } else {
       message.reply("You didn't mention the user to kick!");
     }
@@ -98,50 +90,41 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-  // Ignore messages that aren't from a guild
+  
   if (!message.guild) return;
 
-  // if the message content starts with "!ban"
+  
   if (message.content.startsWith('!ban')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    
     const user = message.mentions.users.first();
-    // If we have a user mentioned
+    
     if (user) {
-      // Now we get the member from the user
+      
       const member = message.guild.member(user);
-      // If the member is in the guild
+      
       if (member) {
-        /**
-         * Ban the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         * Read more about what ban options there are over at
-         * https://discord.js.org/#/docs/main/master/class/GuildMember?scrollTo=ban
-         */
+        
         member
           .ban({
             reason: 'They were bad!',
           })
           .then(() => {
-            // We let the message author know we were able to ban the person
+            
             message.reply(`Successfully banned ${user.tag}`);
             console.log(`banned ${user.tag}`)
           })
           .catch(err => {
-            // An error happened
-            // This is generally due to the bot not being able to ban the member,
-            // either due to missing permissions or role hierarchy
+            
             message.reply('I was unable to ban the member');
-            // Log the error
+            
             console.error(err);
           });
       } else {
-        // The mentioned user isn't in this guild
+        
         message.reply("That user isn't in this guild!");
       }
     } else {
-      // Otherwise, if no user was mentioned
+      
       message.reply("You didn't mention the user to ban!");
     }
   }
